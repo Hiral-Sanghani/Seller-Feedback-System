5e935970968b2062469fa1d10cf20ebd9592400c
Hiral-Sanghani <sanghanihiral1211@gmail.com>
2024-08-28 19:38:30 +0530

    Initial Commit

---
+++ src/main/java/system/feedback/seller/SellerFeedbackSystemApplication.java
@@ -0,0 +1,13 @@
+package system.feedback.seller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SellerFeedbackSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SellerFeedbackSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/system/feedback/seller/SellerFeedbackSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package system.feedback.seller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SellerFeedbackSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
