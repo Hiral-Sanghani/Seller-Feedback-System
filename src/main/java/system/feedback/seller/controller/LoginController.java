e0ea733dbc8ad5101b8a3ecfc6dce538f9bcdd56
Hiral-Sanghani <sanghanihiral1211@gmail.com>
2024-08-30 16:15:31 +0530

    cross origin annotation added in feedback controller and login controller

--- src/main/java/system/feedback/seller/controller/LoginController.java
+++ src/main/java/system/feedback/seller/controller/LoginController.java
@@ -1,14 +1,13 @@
 package system.feedback.seller.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.AutoConfigurationPackage;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import system.feedback.seller.model.User;
 import system.feedback.seller.service.LoginService;
 
-@RestController
+@RestController 
+@CrossOrigin(origins = "http://localhost:3000") 
 @RequestMapping("/api")
 public class LoginController {
 
