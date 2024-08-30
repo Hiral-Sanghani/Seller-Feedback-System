8ce922467e729489d588d5eaf2146b76336a84c8
Hiral-Sanghani <sanghanihiral1211@gmail.com>
2024-08-30 15:19:12 +0530

    added code to return some default feedbacks

--- src/main/java/system/feedback/seller/service/FeedbackServiceImpl.java
+++ src/main/java/system/feedback/seller/service/FeedbackServiceImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Service;
 import system.feedback.seller.model.Feedback;
 import system.feedback.seller.repository.FeedbackRepository;
 
-import javax.swing.text.html.Option;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -24,7 +24,13 @@ public class FeedbackServiceImpl implements FeedbackService {
 
     @Override
     public List<Feedback> fetchAllFeedbacks() {
-        return feedbackRepository.findAll();
+        List<Feedback> list = new ArrayList<Feedback>();
+        
+        list.add(new Feedback(1L, "Int Solutions", 3, "Reviews"));
+        list.add(new Feedback(2L, "Pari", 5, ""));
+        list.add(new Feedback(3L, "Aahan", 1, "No commnets"));
+        // return feedbackRepository.findAll();
+        return list;
     }
 
     @Override
